5f4080c7e5b81dcf0494d0cbe3d7cc3cab072e56
cowboybepopwhite <2224023737@qq.com>
2024-08-19 17:44:27 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -91,8 +91,9 @@ document.getElementById('submit-botton').addEventListener('click', () => {
             finishedQuestion++;
         }
         checkAnswer();
+        indexnow = currentQuestionIndex;
     }
-    indexnow = currentQuestionIndex;
+
 });
 
 
